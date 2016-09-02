7ced70a94b5ce20b47fbdfbd33924a5453e9dd84
Shane Whithead <swhitehead@gmail.com>
2016-09-02 16:20:59 +1000

    Implement testing

--- TestPromises/ViewController.swift
+++ TestPromises/ViewController.swift
@@ -11,6 +11,8 @@ import PromiseKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var toggle: UISwitch!
+	@IBOutlet weak var activity: UIActivityIndicatorView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +23,18 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func makeItSo(_ sender: AnyObject) {
+		activity.startAnimating()
+		
+		_ = firstly { () -> Promise<Bool> in
+				Settings.shared.promoise()
+			}.then(execute: { (result: Bool) -> Void in
+				print(result)
+				self.toggle.isOn = result
+			}).always {
+				self.activity.stopAnimating()
+		}
+	}
 
 }
 
@@ -28,7 +42,19 @@ class Settings {
 	static var shared = Settings()
 	
 	func load() -> Bool {
-		sleep(10)
+		for index in 0..<10 {
+			print(index)
+			sleep(1)
+		}
 		return true
+	}
+	
+	func promoise() -> Promise<Bool> {
+		return Promise<Bool> { (fulfill, error) in
+			DispatchQueue.global().async {
+				let result = self.load()
+				fulfill(result)
+			}
+		}
 	}
 }
